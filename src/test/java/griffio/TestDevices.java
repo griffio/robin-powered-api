3c1349ee2544b5d613711d66ffc7de49af9a7e5d
griffio <griffio@users.noreply.github.com>
2015-03-09 21:25:50 +0000

    test

--- src/test/java/griffio/TestDevices.java
+++ src/test/java/griffio/TestDevices.java
@@ -3,7 +3,7 @@ package griffio;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import griffio.robinpowered.resources.DeviceResource;
-import griffio.robinpowered.resources.gson.DeviceResourceDeserializer;
+import griffio.gson.DeviceResourceDeserializer;
 import org.testng.annotations.Test;
 
 public class TestDevices {
